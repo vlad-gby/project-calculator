4091f218452521c8ffd2fccce878a9703f2d7ae3
Vladyslav Hubanov <155400906+vlad-gby@users.noreply.github.com>
2024-02-19 21:57:08 +0200

    Write a multi-operational calculator

--- script.js
+++ script.js
@@ -43,7 +43,6 @@ function mousedownHandler() {
   this.style.backgroundColor = `rgb(${R - 20}, ${G - 20}, ${B - 20})`;
 }
 function mouseupHandler() {
-  console.log(5)
   const R = Number(getComputedStyle(this).backgroundColor.slice(4, 7));
   const G = Number(getComputedStyle(this).backgroundColor.slice(9, 12));
   const B = Number(getComputedStyle(this).backgroundColor.slice(14, 17));
@@ -57,71 +56,85 @@ function mouseupNumberHandler() {
     display();
   }
 
-  if(!num1) {
-    num1 = Number(this.textContent);
-  }else if(num1 && !operator){
-    num1 = Number(String(num1) + this.textContent);
-  }else if(operator && !num2) {
-    num2 = Number(this.textContent);
-  }else if(num2) {
-    num2 = Number(String(num2) + this.textContent)
+  if(!nums[0]){
+    output.textContent = output.textContent + String(this.textContent);
+    nums.push(Number(this.textContent));
+  } else if(nums.length - operators.length === 1){
+    nums[nums.length - 1] = Number(String(nums.at(-1)) + this.textContent);
+    output.textContent = output.textContent + String(this.textContent);
+    console.log();
+  } else if(nums.length == operators.length){
+    output.textContent = output.textContent + ' ' + String(this.textContent);
+    nums.push(Number(this.textContent));
   }
 
-  display();
+  console.log(nums);
 }
 function mouseupOperatorHandler() {
-  if(!operator) operator = this.textContent;
-  display();
+  if(!nums[0]) return;
+  operators.push(this.textContent);
+
+  output.textContent = output.textContent + ' ' + String(this.textContent);
 }
 
 
-let num1;
-let operator;
-let num2;
+let nums = [];
+let operators = [];
 let result;
 
-
-function display(){
-  if(result) {
-    output.textContent = `${result}`;
-    return;
-  }
-
-  if(num1){
-    output.textContent = `${num1}`;
-    if(operator){
-      output.textContent = `${num1} ${operator}`;
-      if(num2){
-        output.textContent = `${num1} ${operator} ${num2}`
-      }
-    }
-  }
-  if(!num1) output.textContent = '';
-}
-
 function clear(){
-  num1 = num2 = operator = result = 0;
-  display();
+  nums = [];
+  operators = [];
+  result = 0;
+  output.textContent = '';
 }
 
-// =========ATTENTION=====OPERATOR AND THEN A NUMBER??======
 function equals(){
-  if(!num1) return;
-  switch(operator){
-    case '+': result = num1 + num2;
-    break;
-    case '-': result = num1 - num2;
-    break;
-    case '*': result = num1 * num2;
-    break;
-    case '/':
-      result = num1 / num2 
-      if(result !== Math.round(100*(num1 / num2)) / 100){
-        result = `~${Math.round(100*(num1 / num2)) / 100}`
-      }
-    break;
-  }
-  display();
-}
+  // NO CALCLULATIONS
+  if(!nums[0]) {
+    result = 'Nothing entered';
+    display();
+    return;
+  };
+  if(nums.length === 1) {
+    result = nums[0];
+    display();
+    return;
+  };
 
+  // EXECUTE MULTIPLICATIONS AND DIVISIONS
+  for(let i = 0; i < operators.length; i++){
+    if(operators[i] === '*'){
+      nums.splice(i, 2, nums[i] * nums[i + 1]);
+      operators.splice(i, 1);
+      i--;
+    }
+    if(operators[i] === '/'){
+      nums.splice(i, 2, nums[i] / nums[i + 1]);
+      operators.splice(i, 1);
+      i--;
+    }
+  };
+  console.log(nums);
+  console.log(operators);
+
+  // ADD EVERYTHIGN ELSE UP
+  result = operators.reduce((result, operator, index) => {
+    if(operator === '+'){
+      result += nums[index + 1];
+    }
+    if(operator === '-'){
+      result -= nums[index + 1];
+    }
+    return result;
+  }, nums[0]);
+
+  // CHECK IF A BIG DECIMAL
+  if(result !== Math.round(result * 100) / 100){
+    result = '~' + String(Math.round(result * 100) / 100);
+  }
+  console.log(result);
+
+  output.textContent = result;
+}
 
